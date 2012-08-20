a19c0bed0996b64b5c33f1f416257581c4d6e0a9
HAYASHI Kentaro <kenhys@gmail.com>
2012-08-20 19:51:25 +0900

    add initial header

---
+++ src/prefs_filter.h
@@ -0,0 +1,10 @@
+/*
+ * Prefs-filter plugin for Sylpheed -- 
+ * Copyright (C) 2012 HAYASHI Kentaro <kenhys@gmail.com>
+ *
+ */
+
+#ifndef __PREFS_FILTER_H__
+#define __PREFS_FILTER_H__
+
+#endif /* __PREFS_FILTER_H__ */
