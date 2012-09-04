a4d773fcbe7c033d08d5e5664a3c592bc4931a1c
HAYASHI Kentaro <kenhys@gmail.com>
2012-09-04 21:33:17 +0900

    remove needless declaration

--- src/prefs_filter.h
+++ src/prefs_filter.h
@@ -26,13 +26,8 @@ static void textview_menu_popup_cb(GObject *obj, GtkMenu *menu,
                                    const gchar *selected_text,
                                    MsgInfo *msginfo);
 
-static void menu_selected_cb(void);
+static void prefs_filter_menu_cb(void);
 
-static void compose_created_cb(GObject *obj, gpointer compose);
-static void compose_destroy_cb(GObject *obj, gpointer compose);
-static gboolean compose_send_cb(GObject *obj, gpointer compose,
-				gint compose_mode, gint send_mode,
-				const gchar *msg_file, GSList *to_list);
 static void messageview_show_cb(GObject *obj, gpointer msgview,
 				MsgInfo *msginfo, gboolean all_headers);
 static void inc_start_cb(GObject *obj, PrefsAccount *ac);
@@ -40,7 +35,5 @@ static void inc_finished_cb(GObject *obj, gint new_messages);
 
 static void prefs_filter_open_cb(GObject *obj, PrefsAccount *ac);
 static void prefs_filter_edit_open_cb(GObject *obj, PrefsAccount *ac);
-static void create_window(void);
-static void create_folderview_sub_widget(void);
 
 #endif /* __PREFS_FILTER_H__ */
